dde8c89b72410b2eb312317492e5d01c02c75309
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-14 18:53:46 +0530

    [Mukul Dev] Add; This is a Stack and Queue problem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueueProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Stack And Queue Problem");
+        }
+    }
+}
