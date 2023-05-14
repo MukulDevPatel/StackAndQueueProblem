92c2008e5ec34b5a100eeb1e97b5dd16e5cd9a5b
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-14 19:19:36 +0530

    [Mukul Dev] Add; Create a program and add to integer value using Stack

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Stack And Queue Problem");
+            Console.WriteLine("Stack Problem");
+            LinkedList stack = new LinkedList();
+            stack.Add(56);
+            stack.Add(30);
+            stack.Add(70);
+            stack.Display();
         }
     }
 }
